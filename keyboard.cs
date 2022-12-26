8c12155e9260806ee929f05374d14ec581147d25
C1ayemore <isip_a.a.bokov@mpt.ru>
2022-12-26 16:12:39 +0300

    zxccc

---
+++ keyboard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileObcerver
+{
+    internal class keyboard
+    {
+        public delegate void KeyHandler(ConsoleKey consoleKey); //Создание делегата и подвязка события
+        public event KeyHandler OnChange;
+        public void keyControl()
+        {
+            ConsoleKey consoleKey;
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                consoleKey = keyInfo.Key;
+
+                OnChange?.Invoke(consoleKey); //Событие на каждое нажатие кнопки
+            }
+        }
+    }
+}
